1fc94fb5b78d994817a7ba200a4bfdb908605ed3
ariveamar <41986809+ariveamar@users.noreply.github.com>
2022-05-31 10:37:11 +0700

    Update 1-2-0.sh

--- custom_baseline_hardening_demo/roles/rhel_scan/files/1-2-0.sh
+++ custom_baseline_hardening_demo/roles/rhel_scan/files/1-2-0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [[ $(grep -c -e "^nameserver 10.1.12.1$" /etc/resolv.conf) -eq 1  && $(grep -c -e "^search siloamhospitals.com$" /etc/resolv.conf) -eq 1 ]]; then
+if [[ $(grep -c -e "^nameserver 10.8.230.71$" /etc/resolv.conf) -eq 1  && $(grep -c -e "^search siloamhospitals.com$" /etc/resolv.conf) -eq 1 ]]; then
     exit 0
 else
     exit 1
